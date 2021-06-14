5b819023650f05d02580cae9f64e58b7bf18a989
Amani Touihri <44845156+amenytouihri@users.noreply.github.com>
2021-06-14 18:55:27 +0100

    Add files via upload

---
+++ Tasks Manager/Project_TaskManager/src/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+	public static void main(String [] args) {
+	//Displaying The animation	
+		Animation a = new Animation(1000);
+		a.showSplashAndExit();
+		Tasks t = new Tasks();
+		t.main(null);
+		
+	}
+}
